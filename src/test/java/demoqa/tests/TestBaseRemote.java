3512ea5752e322e5d4b1ba910c894dac6d789296
Anastasiia Belova <nastasia_belova@mail.ru>
2023-07-15 15:23:14 +0300

    add selenoid

--- src/test/java/demoqa/tests/TestBaseRemote.java
+++ src/test/java/demoqa/tests/TestBaseRemote.java
@@ -1,7 +1,13 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBaseRemote {
 
@@ -11,6 +17,22 @@ public class TestBaseRemote {
         Configuration.browserSize = "1920x1080";
        // Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
