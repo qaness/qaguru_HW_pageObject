0a442098220f96243ea24fba80b17992d6da8a28
Anastasiia Belova <nastasia_belova@mail.ru>
2023-07-15 14:36:58 +0300

    created copy for remote tests

---
+++ src/test/java/demoqa/tests/RegistrationTestRemote.java
@@ -0,0 +1,56 @@
+package demoqa.tests;
+
+import demoqa.pages.RegistrationPage;
+import demoqa.utils.TestsRandomData;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationTestRemote extends TestBaseRemote{
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    TestsRandomData randomValue = new TestsRandomData();
+
+    @Test
+    void successfulRegistrationTest() {
+
+        String picture = "example.png";
+
+        registrationPage.openPage()
+                //Input fields
+                .setFirstName(randomValue.firstName)
+                .setLastName(randomValue.lastName)
+                .setUserEmail(randomValue.userEmail)
+                .setUserNumber(randomValue.userNumber)
+                .setCurrentAddress(randomValue.currentAddress)
+
+                //Radio & checkbox
+                .setGender(randomValue.gender)
+                .setHobbies(randomValue.hobbies)
+
+                // Date & drop-down
+                .setBirthDay(randomValue.birthdayDay, randomValue.birthdayMonth, randomValue.birthdayYear)
+                .setSubjects(randomValue.subject)
+                .setStateAndCity(randomValue.state, randomValue.city)
+
+                //Upload Image field
+                .uploadPicture(picture)
+
+                //submit
+                .submitForm()
+
+                // assertions
+                .checkResultFormAppeared()
+                .validateItemsInForm("Student Name", randomValue.firstName + " " + randomValue.lastName)
+                .validateItemsInForm("Student Email", randomValue.userEmail)
+                .validateItemsInForm("Gender", randomValue.gender)
+                .validateItemsInForm("Mobile", randomValue.userNumber)
+                .validateItemsInForm("Date of Birth", randomValue.birthdayDay +
+                                    " " + randomValue.birthdayMonth +
+                                    "," + randomValue.birthdayYear)
+                .validateItemsInForm("Subjects", randomValue.subject)
+                .validateItemsInForm("Hobbies", randomValue.hobbies)
+                .validateItemsInForm("Picture", picture )
+                .validateItemsInForm("Address", randomValue.currentAddress)
+                .validateItemsInForm("State and City", randomValue.state + " " + randomValue.city);
+    }
+
+}
